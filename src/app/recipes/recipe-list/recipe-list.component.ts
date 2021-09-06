39f72158abdb72cd38be032d08e15689efb5ad1f
mbrie041 <mbrie041@gmail.com>
2021-09-06 18:13:50 -0400

    Start on the ShoppingListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'
     ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test',
+      'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'
+    ),
   ];
 
   constructor() {}
