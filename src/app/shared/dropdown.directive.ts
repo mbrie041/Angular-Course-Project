d694a5466ba80b32e72f04e96f7b3455fc896de3
mbrie041 <mbrie041@gmail.com>
2021-09-08 18:51:31 -0400

    build a drop down menu using a directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { FactoryTarget } from '@angular/compiler';
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
